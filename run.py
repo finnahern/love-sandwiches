7378d8fa03ae3189606c4ecad659121281d27dd2
finnahern <finna9x@gmail.com>
2021-07-31 14:59:59 +0000

    Add main function to call all other functions

--- run.py
+++ run.py
@@ -64,8 +64,14 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
 
+def main():
+    """
+    Run all program functions
+    """
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
 
-update_sales_worksheet(sales_data)
+print("Welcome to Love Sandwiches Data Automation")
+main()
